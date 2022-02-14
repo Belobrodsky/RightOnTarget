34d2e2a1a1667a6ab80ef2e5e084d986134aae9c
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-02-14 18:04:27 +0300

    everyThinkWorksFine

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -9,11 +9,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var UISlider: UISlider!
+
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet weak var currentPointsLabel: UILabel!
+    
+    @IBOutlet weak var totalPointsLabel: UILabel!
+    
+    //  загаданное число
+    var number:Int=0
+    
+// round
+    var round:Int=0
+    
+     //сумма очков
+    var points: Int=0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func checkNumber(_ sender: Any) {
+     
+        if self.round==0{
+            
+            self.number = Int.random(in: 1...50)
+            self.label.text = String(self.number)
+            self.round=1
+            
+            
+        }
+        else
+        {
+            let numSlider = Int(self.slider.value.rounded())
+            
+            switch self.number {
+            case numSlider:
+                self.points+=50
+            case 1...numSlider:
+                self.points+=50-numSlider+self.number
+            case numSlider...50:
+                self.points+=50-self.number+numSlider
+            default:
+                self.points+=0
+            }
+            
+            currentPointsLabel.text = String("Current:    \(numSlider)")
+            totalPointsLabel.text = String("Total: \(self.points)")
+            
+            
+            
+            
+            if self.round == 5
+            {
+                let alert = UIAlertController(title: "Игра окончена", message:  "Вы заработали \(self.points) очков", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Начать заново", style: UIAlertAction.Style.default,handler: nil))
+                self.present(alert, animated: true) {
+                    //
+                }
+                
+                self.round=1
+                self.points=0
+                
+                self.slider.value = 25
+                
+            }
+            else {self.round+=1}
+            
+            self.number = Int.random(in: 1...50)
+            self.label.text = String(self.number)
+            
+            
+        }
+    
+        
+        
+        
+    }
 }
 
